44f0a75a970527243bc4ec4d78acf8e9050955f3
geogrey12muiruri <greggambugua@gmail.com>
2024-08-08 16:03:36 +0300

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 import Header from "./components/header/Header";
 import Hero from "./components/Hero";
 // import Pricing from "./components/Pricing";
@@ -21,7 +21,7 @@ const Layout = ({ children }) => (
     <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
       <Header />
       {children}
-      {/* <Footer /> Uncomment if you want the Footer to be part of the Layout */}
+      <Footer /> Uncomment if you want the Footer to be part of the Layout
     </div>
     <ButtonGradient />
   </>
