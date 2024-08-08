6e997d1e130c7f4373b8cab39f6c8de3563eb8f2
geogrey12muiruri <greggambugua@gmail.com>
2024-08-08 18:43:14 +0300

    minor adjustments on the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,8 +6,9 @@ const Footer = () => {
     <footer className="bg-slate-950 rounded-lg shadow dark:bg-gray-900 m-4">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a href="https://yourwebsite.com" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+          <a href="#" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
             <img src={parrotLogo} className="h-8" alt="Parrot Logo" />
+            <span className='animate-pulse text-sm text-orange-500 dark:text-orange-400'>ParrotDevs</span>
 
           </a>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
